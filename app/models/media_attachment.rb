6ae4bf5ef9787f68651b207357780b32a583135b
Marek Lipka <mlipka@efigence.com>
2019-09-04 20:43:17 +0200

    Prevent stripping color profiles from JPEGs

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -157,7 +157,7 @@ class MediaAttachment < ApplicationRecord
   }.freeze
 
   GLOBAL_CONVERT_OPTIONS = {
-    all: '-quality 90 -strip +set modify-date +set create-date',
+    all: '-quality 90 +profile exif +set modify-date +set create-date',
   }.freeze
 
   belongs_to :account,          inverse_of: :media_attachments, optional: true
