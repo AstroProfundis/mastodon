db868d519c0b4795c1ca9ea33c19dcb97959ac4f
noellabo <noel.yoshiba@gmail.com>
2019-11-29 12:10:59 +0900

    Apply quote by parsing post body

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -31,8 +31,8 @@ class PostStatusService < BaseService
     return idempotency_duplicate if idempotency_given? && idempotency_duplicate?
 
     validate_media!
-    preprocess_quote!
     preprocess_attributes!
+    preprocess_quote!
 
     if scheduled?
       schedule_status!
@@ -55,6 +55,7 @@ class PostStatusService < BaseService
 
   def quote_from_url(url)
     return nil if url.nil?
+
     quote = ResolveURLService.new.call(url)
     status_from_uri(quote.uri) if quote
   rescue
@@ -68,8 +69,8 @@ class PostStatusService < BaseService
     @visibility   = :unlisted if @visibility&.to_sym == :public && @account.silenced?
     @scheduled_at = @options[:scheduled_at]&.to_datetime
     @scheduled_at = nil if scheduled_in_the_past?
-    if @options[:quote_id].nil? && md = @text.match(/QT:\s*\[\s*(https:\/\/.+?)\s*\]/)
-      @options[:quote_id] = quote_from_url(md[1])&.id
+    if @quote_id.nil? && md = @text.match(/QT:\s*\[\s*(https:\/\/.+?)\s*\]/)
+      @quote_id = quote_from_url(md[1])&.id
       @text.sub!(/QT:\s*\[.*?\]/, '')
     end
   rescue ArgumentError
