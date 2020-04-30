2c6271b01a6b1831b8251845d3839e2dc97ccf46
wuyingren <i@bgme.me>
2020-04-30 10:30:06 +0800

    convert Chinese characters from Traditional to Simplified

--- app/chewy/tags_index.rb
+++ app/chewy/tags_index.rb
@@ -3,11 +3,20 @@
 class TagsIndex < Chewy::Index
   include DatetimeClampingConcern
 
-  settings index: { refresh_interval: '30s' }, analysis: {
+  settings index: { refresh_interval: '3m' }, analysis: {
+    char_filter: {
+      tsconvert: {
+        type: 'stconvert',
+        keep_both: false,
+        delimiter: '#',
+        convert_type: 't2s',
+      },
+    },
     analyzer: {
       content: {
         tokenizer: 'ik_max_word',
         filter: %w(lowercase asciifolding cjk_width),
+        char_filter: %w(tsconvert),
       },
 
       edge_ngram: {
