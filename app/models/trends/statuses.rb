581586fc15c7bafafca931faf8b0a7e96f09c028
Allen Zhong <allen@moe.cat>
2022-03-15 18:52:36 +0800

    adjust trending decay

--- app/models/trends/statuses.rb
+++ app/models/trends/statuses.rb
@@ -4,7 +4,7 @@ class Trends::Statuses < Trends::Base
   PREFIX = 'trending_statuses'
 
   self.default_options = {
-    threshold: 5,
+    threshold: 4,
     review_threshold: 3,
     score_halflife: 2.hours.freeze,
     decay_threshold: 0.3,
