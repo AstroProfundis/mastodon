df09bce8c87b3b07818d7a7fccaa5ff63ba6a289
mashirozx <adadam@qq.com>
2022-04-01 11:02:01 +0800

    fix :quotation

--- app/lib/text_formatter.rb
+++ app/lib/text_formatter.rb
@@ -68,18 +68,6 @@ class TextFormatter
     end
   end
 
-  def format_in_quote(status, **options)
-    html = to_s(status)
-    return '' if html.empty?
-    doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
-    doc.search('span.invisible').remove
-    html = doc.css('body')[0].inner_html
-    html.sub!(/^<p>(.+)<\/p>$/, '\1')
-    html = Sanitize.clean(html).delete("\n").truncate(150)
-    html = encode_custom_emojis(html, status.emojis) if options[:custom_emojify]
-    html.html_safe
-  end
-
   private
 
   def rewrite
--- app/models/status.rb
+++ app/models/status.rb
@@ -39,6 +39,8 @@ class Status < ApplicationRecord
   include StatusSnapshotConcern
   include RateLimitable
 
+  include FormattingHelper
+
   rate_limit by: :account, family: :statuses
 
   self.discard_column = :deleted_at
@@ -263,6 +265,11 @@ class Status < ApplicationRecord
     fields += preloadable_poll.options unless preloadable_poll.nil?
 
     @emojis = CustomEmoji.from_text(fields.join(' '), account.domain) + (quote? ? CustomEmoji.from_text([quote.spoiler_text, quote.text].join(' '), quote.account.domain) : [])
+  end
+
+  def index_text
+    # @index_text ||= [spoiler_text, Formatter.instance.plaintext(self)].concat(media_attachments.map(&:description)).concat(preloadable_poll ? preloadable_poll.options : []).concat(quote? ? ["QT: [#{quote.url || ActivityPub::TagManager.instance.url_for(quote)}]"] : []).filter(&:present?).join("\n\n")
+    @index_text ||= [spoiler_text, quote_status_content_format(self)].concat(media_attachments.map(&:description)).concat(preloadable_poll ? preloadable_poll.options : []).concat(quote? ? ["QT: [#{quote.url || ActivityPub::TagManager.instance.url_for(quote)}]"] : []).filter(&:present?).join("\n\n")
   end
 
   def ordered_media_attachments
