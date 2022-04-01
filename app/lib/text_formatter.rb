a5b83f75939f45ac1d16d793e1ca4959d3327f86
mashirozx <adadam@qq.com>
2022-04-01 11:02:01 +0800

    fix :quotation

--- app/lib/text_formatter.rb
+++ app/lib/text_formatter.rb
@@ -48,18 +48,6 @@ class TextFormatter
     html.html_safe # rubocop:disable Rails/OutputSafety
   end
 
-  def format_in_quote(status, **options)
-    html = to_s(status)
-    return '' if html.empty?
-    doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
-    doc.search('span.invisible').remove
-    html = doc.css('body')[0].inner_html
-    html.sub!(/^<p>(.+)<\/p>$/, '\1')
-    html = Sanitize.clean(html).delete("\n").truncate(150)
-    html = encode_custom_emojis(html, status.emojis) if options[:custom_emojify]
-    html.html_safe
-  end
-
   private
 
   def rewrite
